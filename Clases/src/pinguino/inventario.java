796c7a04c840c5dedc50a55c717919182f25382b
Dgarcia05 <danielgarciamartin6@alumnes.ilerna.com>
2025-04-07 17:25:47 +0200

    Update inventario.java

--- Clases/src/pinguino/inventario.java
+++ Clases/src/pinguino/inventario.java
@@ -1,5 +1,21 @@
 package pinguino;
 
 public class inventario {
+private ArrayList<BolasDeNieve> bolasdenieve;
+private ArrayList<Pescado> pescado;
 
+	    public Inventario() {
+	        this.equipamiento = new ArrayList<>();
+	    }
+
+	    public void agregarInventario(Inventario inventario) {
+	        this.inventario.add(equipamiento);
+	    }
+
+	    public void mostrarInventario() {
+	        System.out.println("Inventario:");
+	        for (Inventario i : inventario) {
+	            System.out.println(i.getNombre() + " - " + i.getTipo());
+	        }
+	}
 }
