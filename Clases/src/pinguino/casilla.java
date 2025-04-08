11f9a5dff9ddaaeb9720b45a23769ac31f66e3d7
Victor2633809 <VICTORCHAVARRIACOLEA2@GMAIL.COM>
2025-04-08 18:42:46 +0200

    Update casilla.java

--- Clases/src/pinguino/casilla.java
+++ Clases/src/pinguino/casilla.java
@@ -1,8 +1,33 @@
 package pinguino;
-
+import java.util.random;
 public class casilla {
+	//Declaramos las variables
+	private static int casilla;
+	private static int casilla agujero;
+	private static int casilla trineo;
+	private static int casilla evento;
+	private static int casilla normal;
+	private static Random rand= New Random;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Declaramos el tipo de casilla aguejero
+	public static void casilla ajuguero ()
+		{
+		resultado= casilla aguejero;
+		resultado=Ran.nextInt;
+	}
+//Declaramos el tipo de la casilla trineo
+	public static void casilla trineo
+		{
+		resultado=casilla trineo;
+		resultado=Ran.nextInt;
+	}
+//Declaramos el tipo de casilla normal
+	public static void casilla normal 
+{
+	resultado= casilla normal;
+	resultado= Ran.nextInt;
+	}
+		
 	}
 }
