06103688bb77585e06c7c5ef7a1fa1c96023ca50
kkkarolina12 <kliminakaro@alumnes.ilerna.com>
2025-04-29 19:26:05 +0200

    Update jugador.java

--- Clases/src/pinguino/jugador.java
+++ Clases/src/pinguino/jugador.java
@@ -2,9 +2,23 @@ package pinguino;
 
 public class jugador {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String nombre;
+    private Inventario inventario;
 
-	}
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.inventario = new Inventario();
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Inventario getInventario() {
+        return inventario;
+    }
+
+    public void mostrarInventario() {
+        System.out.println("Inventario de " + nombre + ": " + inventario);
+    }
 }
