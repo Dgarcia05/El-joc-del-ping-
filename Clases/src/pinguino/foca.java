0548b4bc2ee96eb434b5cf47f1e6090ab9728104
kkkarolina12 <kliminakaro@alumnes.ilerna.com>
2025-05-05 16:44:58 +0200

    Update foca.java

--- Clases/src/pinguino/foca.java
+++ Clases/src/pinguino/foca.java
@@ -1,10 +1,44 @@
 package pinguino;
 
 public class foca {
+    private boolean soborno;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Foca() {
+        this.soborno = false;
+    }
 
-	}
+    public boolean isSoborno() {
+        return soborno;
+    }
 
+    public void setSoborno(boolean soborno) {
+        this.soborno = soborno;
+    }
+
+    // El jugador es aplastado: vuelve al inicio
+    public void aplastarJugador(Pingüino p) {
+        if (!soborno) {
+            p.setPosicion(0);
+            System.out.println("¡La foca ha aplastado al pingüino! Regresa al inicio.");
+        } else {
+            System.out.println("La foca fue sobornada. No aplasta al pingüino.");
+        }
+    }
+
+    // El jugador es golpeado: retrocede 3 casillas
+    public void golpearJugador(Pingüino p) {
+        if (!soborno) {
+            int nuevaPos = Math.max(0, p.getPosicion() - 3);
+            p.setPosicion(nuevaPos);
+            System.out.println("¡La foca golpeó al pingüino! Retrocede 3 casillas.");
+        } else {
+            System.out.println("La foca fue sobornada. No golpea al pingüino.");
+        }
+    }
+
+    // Activar soborno
+    public void esSobornado() {
+        this.soborno = true;
+        System.out.println("La foca ha sido sobornada. No atacará al pingüino.");
+    }
 }
